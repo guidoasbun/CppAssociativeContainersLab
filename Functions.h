f00226479cd1dd3a3a51c8f390562c25ae20a2b9
guidoasbun <guido@asbun.io>
2021-11-12 13:15:27 -0800

    Notes

--- Functions.h
+++ Functions.h
@@ -11,6 +11,13 @@
 
 using namespace std;
 
+//Write the definition of the function clusters() that passes an STL multiset of type int and returns the number of clusters.
+//
+//Example:
+//Given the set {1, 2, 2, 3, 4, 4} => clusters 2,2/4,4 => return value is 2
+//Given the set {2, 2, 4, 6, 6, 6, 6, 7, 8, 9, 9, 9} => clusters 2,2/6,6,6,6/9,9,9 => return value is 3
+//
+//Consider the case when the set is empty, to avoid creating any variables unnecessarily.
 int clusters(const multiset<int>& mSet)
 {
     if (mSet.empty())
@@ -33,6 +40,15 @@ int clusters(const multiset<int>& mSet)
     return clusters;
 }
 
+//Write the definition of the function sameKeyValue() that passes a non-empty STL multimap of type int and returns true if all the numbers in the values appear also in the keys.
+//
+//Example:
+//
+//(3,3), (4,4), (5,1), (6,2), (7,3), (7,5) => false
+//The values are 3, 4, 1, 2, 3, and 5, but 1 and 2 are not keys.
+//
+//(1,2), (2,6), (4,2), (4,6), (6,4) => true
+//The values are 2, 6, 2, 6, and 4, and all of them are also keys.
 bool sameKeyValue(const multimap<int, int>& mMap)
 {
     auto iter = mMap.begin();
@@ -47,6 +63,13 @@ bool sameKeyValue(const multimap<int, int>& mMap)
     return true;
 }
 
+//Write the definition of the function tenMultiplesOf() that passes an empty STL set of type int and an int. The function populates the set with 10 multiples of the given integer, except for the values that end with the integer itself.
+//
+//Example:
+//2 => {4, 6, 8, 10, 14, 16, 18, 20, 24, 26}
+//3 => {6, 9, 12, 15, 18, 21, 24, 27, 30, 36}
+//
+//You may use function pow() for your calculations. The header <cmath> is already included.
 void tenMultiplesOf(set<int>& intSet, int integer)
 {
     int intSetCount{0};
